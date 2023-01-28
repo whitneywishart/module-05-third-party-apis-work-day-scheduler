ecab48683b22b95746d7aae183cbdead1347f436
whitneywishart <whitneywishart@gmail.com>
2023-01-28 15:35:55 -0800

    local storage refactor

--- assets/script.js
+++ assets/script.js
@@ -3,37 +3,28 @@ var now = dayjs().format("HH");
 
 //add current date at top of page
 var today = dayjs();
-$("#currentDay").text(today.format("MMM D, YYYY, H:m A"));
+$("#currentDay").text(today.format("MMM D, YYYY, h:m A"));
 
 
 $(function () {
   //add click event listener for save button and send entry value to local storage
-  var saveBtn = document.querySelector("#hour-09 > button > i");
-  var getTextEntry = document.querySelector("#hour-09 > textarea");
-
-  saveBtn.addEventListener("click", function (event) {
+  $(".saveBtn").click(function (event) {
     event.preventDefault();
-
-    //create calendar entry object
-    var entry = {
-      textEntry: getTextEntry.value.trim(),
-    };
-
-    //send entry to local storage
-    console.log(entry);
-    localStorage.setItem("entry", JSON.stringify(entry));
+    var value = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id").split("-")[1];
+    localStorage.setItem(time, value);
 
   })
 
 
-  
+
   //change row color for each hour based on current time
   $(".row").each(function () {
 
     var time = $(this).attr("id").split("-")[1];
     // console.log(this);
     // console.log(now);
-    console.log(time);
+    // console.log(time);
 
 
     if (now == time) {
@@ -51,14 +42,7 @@ $(function () {
       $(this).addClass("past");
 
     }
-  });
-
-
-
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+  })
 
 });
 
